d967ad1494264959f53241aab82b240e65b0efcf
Stacy Chege <stacychege23@gmail.com>
2025-08-19 20:03:48 +0300

    chore: Register all models in the Django admin panel

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import ExpertProfile, WorkoutPlan, UserWorkoutActivity, Like, Comment
 
 # Register your models here.
+admin.site.register(ExpertProfile)
+admin.site.register(WorkoutPlan)
+admin.site.register(UserWorkoutActivity)
+admin.site.register(Like)
+admin.site.register(Comment)
