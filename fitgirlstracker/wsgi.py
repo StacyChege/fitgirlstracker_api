5dd5a9b150d1ab52dcf88376dfc5be8f6b0514bf
Stacy Chege <stacychege23@gmail.com>
2025-08-31 23:38:10 +0300

    feat: Update production settings and add database configuration; create key generation script

--- fitgirlstracker/wsgi.py
+++ fitgirlstracker/wsgi.py
@@ -1,16 +1,18 @@
-"""
-WSGI config for fitgirlstracker project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
+
+# Add your project directory to the sys.path
+path = 'https://www.pythonanywhere.com/user/StacyChege1/files/home/StacyChege1/fitgirlstracker_api'
+if path not in sys.path:
+    sys.path.insert(0, path)
+
+# Set up the Django project environment
+os.environ['DJANGO_SETTINGS_MODULE'] = 'fitgirlstracker_api.settings'
+
+# Activate your virtual environment
+activate_this = '/home/StacyChege1/fitgirlstracker_api/venv/bin/activate_this.py'
+with open(activate_this) as f:
+    exec(f.read(), dict(__file__=activate_this))
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fitgirlstracker.settings')
-
 application = get_wsgi_application()
